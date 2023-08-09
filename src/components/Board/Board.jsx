c2a5f5e97ace367964f6273bea53a257ad2a1683
shivamk1998 <shivamkumarsingh1998@gmail.com>
2023-08-09 21:49:58 +0530

    added Box and board component.

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Board.css'
+
+function Board(){
+  return(
+    <div>Hello</div>
+  )
+}
+
+export default Board;
---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Box.css'
+
+function Box(){
+  return (
+    <div></div>
+  )
+}
+
+export default Box;
